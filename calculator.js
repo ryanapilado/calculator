a81f84a4d711806e39758f9ad21b90182603e02b
Ryan Apilado <ryan.apilado@gmail.com>
2017-02-20 21:57:14 -0500

    implement delete, clear, and fixed exponent

--- calculator.js
+++ calculator.js
@@ -2,6 +2,18 @@ function addChar(char) {
     document.getElementById("usr").value += char;
 }
 
+function del() {
+    var cur = document.getElementById("usr").value;
+    if (cur.length > 0) {
+        document.getElementById("usr").value = cur.slice(0, cur.length - 1);
+    }
+}
+
+function reset() {
+    document.getElementById("usr").value = '';
+}
+
 function calculate() {
-    
+    var result = eval(document.getElementById("usr").value);
+    document.getElementById("usr").value = result;
 }
